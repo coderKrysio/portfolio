73b455cdc72b80b32c5bd26c49588677b2f71615
Anushree <anushreejaiswal.srn@gmail.com>
2024-01-13 23:31:49 +0530

    added loading element

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,21 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Suspense } from "react";
+import Loading from "./loading";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: "Anushree Jaiswal (coderKrysio)",
-    description: "This is the portfolio site of Anushree Jaiswal going with the name coderKrysio",
-	keywords: ["coderKrysio", "Anushree Jaiswal", "frontend developer", "frontend developer in india"]
+    description:
+        "This is the portfolio site of Anushree Jaiswal going with the name coderKrysio",
+    keywords: [
+        "coderKrysio",
+        "Anushree Jaiswal",
+        "frontend developer",
+        "frontend developer in india",
+    ],
 };
 
 export default function RootLayout({
@@ -17,7 +25,9 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" suppressHydrationWarning>
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <Suspense fallback={<Loading />}>{children}</Suspense>
+            </body>
         </html>
     );
 }
