12087e8d9f37f0cf9a6357eb1ccbbd2c22d71d50
Anushree <anushreejaiswal.srn@gmail.com>
2024-11-19 00:42:37 +0530

    landing page design desktop version completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,16 @@
 import { AboutMe } from "@/components/about-me";
 import { ContactMe } from "@/components/contact-me";
-import { Landing } from "@/components/landing";
+import { Landing } from "@/components/landing/landing";
 import { ListingProjects } from "@/components/listing-projects";
 import { NavigationBar } from "@/components/navigation-bar";
 import { Projects } from "@/components/projects";
-import { TempLanding } from "@/components/temp-landing";
 
 export default function Home() {
     return (
-        <main className="h-screen flex flex-col items-center justify-between relative p-5 overflow-hidden bg-[#F9E8D9] text-[#F9E8D9]">
-            <TempLanding />
-            {/* <NavigationBar />
+        <main className="min-h-screen flex flex-col items-center justify-between relative overflow-hidden bg-[#000] text-white">
+            <NavigationBar />
             <Landing />
-            <AboutMe />
+            {/* <AboutMe />
             <Projects />
             <ListingProjects />
             <ContactMe /> */}
