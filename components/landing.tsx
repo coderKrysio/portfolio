d3502c6cf16ecf8ea8aae6996989e00aabcac572
Anushree <anushreejaiswal.srn@gmail.com>
2024-11-18 23:17:31 +0530

    updated the landing page components and placements

--- components/landing.tsx
+++ components/landing.tsx
@@ -1,6 +1,8 @@
 "use client";
-import { Github, Linkedin, Twitter } from "lucide-react";
+
+import { Github, Linkedin, Twitter, Blend } from "lucide-react";
 import { Londrina_Outline, Poppins } from "next/font/google";
+import Image from "next/image";
 import Link from "next/link";
 import { AttentionSeeker, Fade, Flip, Rotate } from "react-awesome-reveal";
 
@@ -16,22 +18,78 @@ const poppins = Poppins({
 
 export const Landing = () => {
     return (
-        <section className={poppins.className}>
-            <div className="flex w-screen h-screen landing--heading px-6">
-                <div className="border-2 border-white w-full">
-                    hi
+        <section className={`relative bg-black z-0 ${poppins.className}`}>
+            <img
+                src={"/images/ai-3.jpg"}
+                alt="background"
+                className="absolute top-0 left-0 object-cover object-top w-screen h-full opacity-70 z-0"
+            />
+            <div className="relative flex w-screen h-screen landing--heading px-6 z-2">
+                <div className="w-full h-full flex justify-center items-center relative">
+                    <div className="absolute top-[80px] left-[80px] w-[200px] h-[200px] rounded-full bg-[#8E050F]/[0.8] outline-dashed outline-2 outline-[#DDD8CF] outline-offset-[-8px]"></div>
+                    <div className="w-[400px] h-[400px] rounded-full outline outline-2 outline-[#DDD8CF] outline-offset-[-16px] bg-[#373737]/[0.6]"></div>
+                    <Blend
+                        width={220}
+                        height={220}
+                        color="#DDD8CF"
+                        strokeWidth={0.1}
+                        className="absolute bottom-[40px] right-[90px]"
+                    />
                 </div>
 
                 <div className="relative w-full h-full flex flex-col justify-between items-end bg-transparent">
+                    <div className="relative h-full flex gap-[10px] mr-[230px]">
+                        <div className="h-[190px] w-[50px] flex justify-center">
+                            <Fade
+                                cascade
+                                damping={0.02}
+                                triggerOnce
+                                className="text-[24px] font-bold text-[#AD886F] whitespace-nowrap select-none"
+                            >
+                                <p className="flex flex-col leading-9">
+                                    {"コ ー ド ク リ シ オ"
+                                        .split(" ")
+                                        .map((ch) => (
+                                            <span>{ch}</span>
+                                        ))}
+                                </p>
+                            </Fade>
+                        </div>
+
+                        <div className="h-[500px] w-[50px] flex justify-center">
+                            <Fade
+                                triggerOnce
+                                className="text-[40px] text-[#3A5D79] whitespace-nowrap select-none"
+                            >
+                                <p className="flex flex-col leading-9">
+                                    {"ア ヌ シ ュ リ ー ・ ジ ャ イ ワ ル"
+                                        .split(" ")
+                                        .map((ch) => (
+                                            <span>{ch}</span>
+                                        ))}
+                                </p>
+                            </Fade>
+                        </div>
+
+                        <div className="h-[600px] w-[50px] flex justify-center items-center">
+                            <Fade
+                                cascade
+                                damping={0.02}
+                                triggerOnce
+                                className="text-[56px] transform rotate-90 text-[#FBB95D] whitespace-nowrap select-none "
+                            >
+                                Full Stack Developer
+                            </Fade>
+                        </div>
+                    </div>
                     <Fade
-                        delay={1300}
                         fraction={0}
                         duration={600}
                         triggerOnce
-                        className="absolute top-[175px] right-[60px]"
+                        className="absolute top-[210px] mr-[-210px]"
                     >
                         <div
-                            className={`text-end text-[216px] rotate-90 leading-[0.9] opacity-30 text-[#FBB95D] fill-white ${lodrina.className}`}
+                            className={`text-[126px] rotate-90 leading-[0.9] text-[#FFE9CA] fill-white`}
                         >
                             <span>
                                 Anushree
@@ -41,42 +99,7 @@ export const Landing = () => {
                         </div>
                     </Fade>
 
-                    <div className="relative h-full flex gap-[10px]">
-                        <div className="h-[190px] w-[50px]  flex justify-center items-center">
-                            <Fade
-                                cascade
-                                damping={0.15}
-                                triggerOnce
-                                className="text-[24px] font-bold transform rotate-90 text-[#562151] whitespace-nowrap select-none"
-                            >
-                                コードクリシオ
-                            </Fade>
-                        </div>
-
-                        <div className="h-[500px] w-[50px] flex justify-center items-center">
-                            <Fade
-                                cascade
-                                damping={0.15}
-                                triggerOnce
-                                className="text-[40px] transform rotate-90 text-[#3F2021] whitespace-nowrap select-none"
-                            >
-                                アヌシュリー・ジャイワル
-                            </Fade>
-                        </div>
-
-                        <div className="h-[600px] w-[50px] flex justify-center items-center">
-                            <Fade
-                                cascade
-                                damping={0.15}
-                                triggerOnce
-                                className="text-[56px] transform rotate-90 text-[#D7713B] whitespace-nowrap opacity-75 select-none"
-                            >
-                                Frontend Developer
-                            </Fade>
-                        </div>
-                    </div>
-
-                    <div className="w-full h-[80px] flex justify-end items-center gap-[20px]">
+                    {/* <div className="w-full h-[80px] flex justify-end items-end gap-[20px]">
                         <Fade
                             delay={2300}
                             direction="right"
@@ -90,8 +113,8 @@ export const Landing = () => {
                                 <Github
                                     width={"25px"}
                                     height={"25px"}
-                                    color="#D7713B"
-                                    className="hover:fill-[#D7713B] opacity-90"
+                                    color="#fffad1"
+                                    className="hover:fill-[#D7713B]"
                                 />
                             </Link>
 
@@ -102,8 +125,8 @@ export const Landing = () => {
                                 <Linkedin
                                     width={"25px"}
                                     height={"25px"}
-                                    color="#D7713B"
-                                    className="hover:fill-[#D7713B] opacity-90"
+                                    color="#fffad1"
+                                    className="hover:fill-[#D7713B]"
                                 />
                             </Link>
 
@@ -114,12 +137,12 @@ export const Landing = () => {
                                 <Twitter
                                     width={"25px"}
                                     height={"25px"}
-                                    color="#D7713B"
-                                    className="hover:fill-[#D7713B] opacity-90"
+                                    color="#fffad1"
+                                    className="hover:fill-[#D7713B]"
                                 />
                             </Link>
                         </Fade>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </section>
