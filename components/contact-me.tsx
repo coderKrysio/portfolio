50c4e0a0562cbb918d73d6f14c4e840fac53ee5c
Anushree <anushreejaiswal.srn@gmail.com>
2024-11-18 23:19:09 +0530

    updated styles and replaced some abstracts

--- components/contact-me.tsx
+++ components/contact-me.tsx
@@ -12,33 +12,34 @@ import { Fade, Roll, Slide } from "react-awesome-reveal";
 
 export const ContactMe = () => {
     return (
-        <section id="contact-me" className="w-full min-h-screen flex flex-col mt-[60px]">
+        <section
+            id="contact-me"
+            className="w-full min-h-screen flex flex-col mt-[60px]"
+        >
             <div className="relative ml-[30px] flex h-screen">
                 <Fade
                     big
                     className="absolute top-[450px] left-[100px] rotate-90"
                 >
                     <div className="text-7xl font-bold text-[#D7713B]">
-                        Contact
+                        Let's
                         <br />
-                        Me
+                        connect
                     </div>
                 </Fade>
 
-                <Roll
-                    className="absolute top-[170px] left-[220px] z-[1]"
-                >
+                <Roll triggerOnce className="absolute top-[170px] left-[220px]">
                     <Squircle
                         width={"200px"}
                         height={"200px"}
                         strokeWidth={0.7}
-                        color="#FB8987"
-                        className="rotate-[30deg]"
+                        color="#e7a43b"
+                        className="rotate-[30deg] opacity-45"
                     />
                 </Roll>
 
                 <Fade className="relative" cascade damping={0.3}>
-                    <div className="absolute top-0 left-0 w-[300px] h-[350px] flex items-center overflow-hidden rounded-lg">
+                    <div className="absolute top-0 left-0 w-[300px] h-[350px] flex items-center overflow-hidden rounded-lg z-[2]">
                         <img
                             src="/images/box-1.jpg"
                             className="w-full bg-cover"
@@ -69,9 +70,10 @@ export const ContactMe = () => {
 
                 <div className="absolute right-[90px] top-[50%] transform translate-y-[-50%] px-[20px]">
                     <Fade direction="right">
-                    <div className="absolute top-[100px] left-[-130px] text-6xl flex flex-col gap-[30px] font-semibold text-[#562151]">
-                        私<br/>
-                    に<br/>連<br/>絡<br/>し<br/>て</div>
+                        <div className="absolute top-[100px] left-[-130px] text-6xl flex flex-col gap-[30px] font-semibold text-[#562151]">
+                            私<br />
+                            に<br />連<br />絡<br />し<br />て
+                        </div>
                     </Fade>
 
                     <Slide
@@ -82,51 +84,51 @@ export const ContactMe = () => {
                     >
                         <Link href={"/"}>
                             <Linkedin
-                                width={"50px"}
-                                height={"50px"}
-                                color="#FBB95D"
+                                width={"30px"}
+                                height={"30px"}
+                                color="#fbb95d"
                                 strokeWidth={0.8}
-                                className="hover:fill-[#FBB95D] mb-[25px]"
+                                className="fill-[#FBB95D] hover:fill-black mb-[25px]"
                             />
                         </Link>
 
                         <Link href={"/"}>
                             <Github
-                                width={"50px"}
-                                height={"50px"}
-                                color="#FBB95D"
+                                width={"30px"}
+                                height={"30px"}
+                                color="#fbb95d"
                                 strokeWidth={0.8}
-                                className="hover:fill-[#FBB95D] mb-[25px]"
+                                className="fill-[#FBB95D] hover:fill-black mb-[25px]"
                             />
                         </Link>
 
                         <Link href={"/"}>
                             <Twitter
-                                width={"50px"}
-                                height={"50px"}
-                                color="#FBB95D"
+                                width={"30px"}
+                                height={"30px"}
+                                color="#fbb95d"
                                 strokeWidth={0.8}
-                                className="hover:fill-[#FBB95D] mb-[25px]"
+                                className="fill-[#FBB95D] hover:fill-black mb-[25px]"
                             />
                         </Link>
 
                         <Link href={"/"}>
                             <Instagram
-                                width={"50px"}
-                                height={"50px"}
-                                color="#FBB95D"
+                                width={"30px"}
+                                height={"30px"}
+                                color="#000"
                                 strokeWidth={0.8}
-                                className="hover:fill-[#FBB95D] mb-[25px] hover:stroke-black"
+                                className="fill-[#FBB95D] hover:fill-black mb-[25px] hover:stroke-[#fbb95d]"
                             />
                         </Link>
 
                         <Link href={"/"}>
                             <Mail
-                                width={"50px"}
-                                height={"50px"}
-                                color="#FBB95D"
-                                strokeWidth={0.8}
-                                className="hover:fill-[#FBB95D] mb-[25px] hover:stroke-black"
+                                width={"30px"}
+                                height={"30px"}
+                                color="#000"
+                                strokeWidth={1}
+                                className="fill-[#FBB95D] hover:fill-black mb-[25px] hover:stroke-[#fbb95d]"
                             />
                         </Link>
                     </Slide>
